f5f375715b3689e7219ab8b5765f7a1afacb1311
barts <bartsteensma@outlook.com>
2022-11-23 10:26:14 +0100

    Finished half of the keymaps

--- Atreus.ino
+++ Atreus.ino
@@ -2,12 +2,13 @@
 #define BUILD_INFORMATION "locally built on " __DATE__ " at " __TIME__
 #endif
 
-#include "Kaleidoscope.h"
-#include "Kaleidoscope-EEPROM-Settings.h"
-#include "Kaleidoscope-EEPROM-Keymap.h"
-#include "Kaleidoscope-FirmwareVersion.h"
-#include "Kaleidoscope-FocusSerial.h"
-#include "Kaleidoscope-Macros.h"
+#include <Arduino.h>
+#include <Kaleidoscope.h>
+#include <Kaleidoscope-EEPROM-Settings.h>
+#include <Kaleidoscope-EEPROM-Keymap.h>
+#include <Kaleidoscope-FirmwareVersion.h>
+#include <Kaleidoscope-FocusSerial.h>
+#include <Kaleidoscope-Macros.h>
 #include "keymaps.h"
 
 KALEIDOSCOPE_INIT_PLUGINS(
--- keymaps.h
+++ keymaps.h
@@ -1,17 +1,12 @@
 #pragma once
 
-#include <Kaleidoscope.h>
-#include <Kaleidoscope-EEPROM-Settings.h>
 #include <Kaleidoscope-EEPROM-Keymap.h>
+#include <Kaleidoscope-EEPROM-Settings.h>
 #include <Kaleidoscope-FirmwareVersion.h>
 #include <Kaleidoscope-FocusSerial.h>
+#include <Kaleidoscope.h>
 
 #define MO(n) ShiftToLayer(n)
-
-#define Br_Decr Consumer_DisplayBrightnessDecrement
-#define Br_Incr Consumer_DisplayBrightnessIncrement
-#define Vol_Decr Consumer_VolumeDecrement
-#define Vol_Incr Consumer_VolumeIncrement
 
 #define Key_Tilde LSHIFT(Key_Backtick)
 #define Key_Exclamation LSHIFT(Key_1)
@@ -31,35 +26,110 @@
 #define Key_RightCurlyBrace LSHIFT(Key_RightBracket)
 #define Key_DubQuote LSHIFT(Key_Quote)
 
+#define Key_BrDecr Consumer_DisplayBrightnessDecrement
+#define Key_BrIncr Consumer_DisplayBrightnessIncrement
+#define Key_VolDecr Consumer_VolumeDecrement
+#define Key_VolIncr Consumer_VolumeIncrement
+
 #define Key_SwitchDisplay LGUI(LSHIFT(Key_RightArrow))
+#define ___ Key_Transparent
+
+#define Key_RCtrl Key_RightControl
+#define Key_RShift Key_RightShift
+#define Key_RAlt Key_RightAlt
+#define Key_RGui Key_RightGui
+#define Key_LCtrl Key_LeftControl
+#define Key_LShift Key_LeftShift
+#define Key_LAlt Key_LeftAlt
+#define Key_LGui Key_LeftGui
+
 enum {
     COLEMAK_DH,
-    SYMBOLS_LEFT,
+    SYMBOLS,
+    NUMPAD,
+    NAVIGATION_RIGHT,
+    NAVIGATION_LEFT,
+    F_KEYS
 };
 
 // clang-format off
 KEYMAPS(
-    [COLEMAK_DH] = KEYMAP_STACKED(
+
+[COLEMAK_DH] = KEYMAP_STACKED(
 
     Key_Q             ,Key_W             ,Key_F           ,Key_P           ,Key_B,
     Key_A             ,Key_R             ,Key_S           ,Key_T           ,Key_G,
     Key_Z             ,Key_X             ,Key_C           ,Key_D           ,Key_V           ,Key_PrintScreen,
-    Br_Incr           ,Br_Decr           ,Key_Escape      ,Key_Tab         ,Key_Space       ,Key_CapsLock
+    Key_BrIncr        ,Key_BrDecr        ,Key_Escape      ,Key_Tab         ,Key_Space       ,Key_CapsLock,
 
-                      ,Key_J             ,Key_L           ,Key_U           ,Key_Y           ,Key_Semicolon
-                      ,Key_M             ,Key_N           ,Key_E           ,Key_I           ,Key_O,
+                       Key_J             ,Key_L           ,Key_U           ,Key_Y           ,Key_Semicolon,
+                       Key_M             ,Key_N           ,Key_E           ,Key_I           ,Key_O,
     Key_Menu          ,Key_K             ,Key_H           ,Key_Comma       ,Key_Period      ,Key_Slash,
-    Key_SwitchDisplay ,Key_Backspace     ,Key_Enter       ,Key_Delete      ,Vol_Decr        ,Vol_Incr),
+    Key_SwitchDisplay ,Key_Backspace     ,Key_Enter       ,Key_Delete      ,Key_VolDecr     ,Key_VolIncr
+    ),
 
-    [SYMBOLS_LEFT] = KEYMAP_STACKED(
+[SYMBOLS] = KEYMAP_STACKED(
 
     Key_Exclamation      ,Key_At             ,Key_Hash        ,Key_Dollar   ,Key_Percent,
     Key_LeftCurlyBrace   ,Key_LeftBracket    ,Key_LeftParen   ,Key_Quote    ,Key_Backtick,
     Key_RightCurlyBrace  ,Key_RightBracket   ,Key_RightParen  ,Key_DubQuote ,Key_Tilde        ,Key_PrintScreen,
-    Br_Incr              ,Br_Decr            ,Key_Escape      ,Key_Equals   ,Key_Underscore   ,Key_Caret
+    ___                  ,___                ,___             ,Key_Equals   ,Key_Underscore   ,Key_Caret,
 
-            ,Key_A   ,Key_A   ,Key_A   ,Key_A   ,Key_A
-            ,Key_A   ,Key_A   ,Key_A   ,Key_A   ,Key_A,
-    Key_A   ,Key_A   ,Key_A   ,Key_A   ,Key_A   ,Key_A,
-    Key_A   ,Key_A   ,Key_A   ,Key_A   ,Key_A   ,Key_A)
+           ___   ,___       ,___        ,___        ,___,
+           ___   ,Key_RCtrl ,Key_RShift ,Key_RAlt   ,Key_RGui,
+    ___   ,___   ,___       ,___        ,___        ,___,
+    ___   ,___   ,___       ,___        ,___        ,___
+    ),
+
+[NUMPAD] = KEYMAP_STACKED(
+
+    ___         ,___        ,___        ,___        ,___,
+    Key_LGui    ,Key_LAlt   ,Key_LShift ,Key_LCtrl  ,___,
+    ___         ,___        ,___        ,___        ,___    ,___,
+    ___         ,___        ,___        ,___        ,___    ,___,
+
+                 ___        ,Key_7      ,Key_8      ,Key_9  ,___,
+                 ___        ,Key_4      ,Key_5      ,Key_6  ,___,
+    ___         ,___        ,Key_1      ,Key_2      ,Key_3  ,___,
+    ___         ,___        ,___        ,___        ,___    ,___
+    ),
+
+[NAVIGATION_RIGHT] = KEYMAP_STACKED(
+
+    ___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___,
+
+           ___   ,___   ,___   ,___   ,___,
+           ___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___
+    ),
+
+[NAVIGATION_LEFT] = KEYMAP_STACKED(
+
+    ___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___,
+
+           ___   ,___   ,___   ,___   ,___,
+           ___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___
+    ),
+
+[F_KEYS] = KEYMAP_STACKED(
+
+    ___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___,
+
+           ___   ,___   ,___   ,___   ,___,
+           ___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___,
+    ___   ,___   ,___   ,___   ,___   ,___
+    )
 )
