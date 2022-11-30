9bf0f3c31f2ae21f778b6bd748fb5d1a4ff97757
barts <b.steensma@fleetcleaner.com>
2022-11-30 11:12:22 +0100

    Fixed coordinates qukeys

--- qukeys.cpp
+++ qukeys.cpp
@@ -8,19 +8,19 @@ using namespace kaleidoscope;
 
 void setup_qukeys(){
   QUKEYS(
-    plugin::Qukey(0, KeyAddr(1, 1), Key_LeftGui),
-    plugin::Qukey(0, KeyAddr(1, 2), Key_LeftAlt),
-    plugin::Qukey(0, KeyAddr(1, 3), Key_LeftShift),
-    plugin::Qukey(0, KeyAddr(1, 4), Key_LeftControl),
+    plugin::Qukey(0, KeyAddr(1, 0), Key_LeftGui),
+    plugin::Qukey(0, KeyAddr(1, 1), Key_LeftAlt),
+    plugin::Qukey(0, KeyAddr(1, 2), Key_LeftShift),
+    plugin::Qukey(0, KeyAddr(1, 3), Key_LeftControl),
 
-    plugin::Qukey(0, KeyAddr(1, 7), Key_RightControl),
-    plugin::Qukey(0, KeyAddr(1, 8), Key_RightShift),
-    plugin::Qukey(0, KeyAddr(1, 9), Key_RightAlt),
-    plugin::Qukey(0, KeyAddr(1, 10), Key_RightGui),
+    plugin::Qukey(-1, KeyAddr(1, 8), Key_RightControl),
+    plugin::Qukey(-1, KeyAddr(1, 9), Key_RightShift),
+    plugin::Qukey(-1, KeyAddr(1, 10), Key_RightAlt),
+    plugin::Qukey(-1, KeyAddr(1, 11), Key_RightGui),
 
-    plugin::Qukey(0, KeyAddr(3, 3), ShiftToLayer(F_KEYS)),
-    plugin::Qukey(0, KeyAddr(3, 4), ShiftToLayer(NAVIGATION_RIGHT)),
-    plugin::Qukey(0, KeyAddr(3, 5), ShiftToLayer(NUMPAD)),
-    plugin::Qukey(0, KeyAddr(3, 8), ShiftToLayer(SYMBOLS)),
-    plugin::Qukey(0, KeyAddr(3, 9), ShiftToLayer(NAVIGATION_LEFT)))
+    plugin::Qukey(0, KeyAddr(3, 2), ShiftToLayer(F_KEYS)),
+    plugin::Qukey(0, KeyAddr(3, 3), ShiftToLayer(NAVIGATION_RIGHT)),
+    plugin::Qukey(0, KeyAddr(3, 4), ShiftToLayer(NUMPAD)),
+    plugin::Qukey(0, KeyAddr(3, 7), ShiftToLayer(SYMBOLS)),
+    plugin::Qukey(0, KeyAddr(3, 8), ShiftToLayer(NAVIGATION_LEFT)))
 }
